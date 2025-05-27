65a7c56679d2a46c6a64081c55c63861ee5de8f2
Noah_Dave <m.davenoa@gmail.com>
2025-05-28 01:35:11 +0300

    project init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'	
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div >
+        <h1 className="text-3xl font-bold underline">
+          E-verse Academy
+        </h1>
+        <p>Welcome to E-verse Academy
+          <br />
+          Your gateway to the metaverse
+        </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
